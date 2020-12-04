424bcbaa9abdb2259cd5cd138975e7906fca1b0d
dayomi02 <ek605500@gmail.com>
2020-12-04 12:26:39 +0900

    비밀번호 암호화

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -32,6 +33,23 @@ const userSchema = mongoose.Schema({
     }
 })
 
+
+userSchema.pre('save', function( next ){
+    var user = this;
+
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function(err, salt){
+            if(err) return nnext(err)
+    
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }    
+})
 const User = mongoose.model('User', userSchema)
 
 module.exports = { User }
