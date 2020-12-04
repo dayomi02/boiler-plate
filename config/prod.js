52ec1f0257921e3a8c5b32a93d3f58839632c8f8
Dayeon-1021 <ek605500@gmail.com>
2020-12-04 10:45:47 +0900

    nodemon다운 &  비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI
+    //내가 설정한 이름으로 해야 함
+}
--- index.js
+++ index.js
@@ -1,16 +1,42 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+const bodyParser = require('body-parser');
 
+const config = require('./config/key');
+
+const { User } = require("./models/User");
+
+//application/x-www-from-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application.json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://user_01:abcd1234@boilerplate.egcwm.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! nodemon다운받음 정보보안함')
+})
+
+
+app.post('/register',(req, res) => {
+
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)   //bodyparser를 통해서 json형식으로 오는 클라이언트의 정보를 받는다.
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
